d38e331715fda69910bd0302c619d26f762e5053
Martin <tincho.pbs43@gmail.com>
2026-03-30 22:51:12 -0300

    practicando-javascript

---
+++ javascript-modulo2/bucles-for-while+if-else/index.js
@@ -0,0 +1,41 @@
+let numero1, numero2,numero3,numero4;
+
+numero1=4;
+numero2=6;
+numero3=8;
+numero4=16;
+
+console.log("dos y dos son:", numero1,"cuatro y dos son:", numero2,"seis y dos son:", numero3,"y ocho:",numero4);
+
+
+let numeros,numeros2;
+
+numeros=[1,2];
+numeros2=[,10,9];
+
+console.log(numeros+numeros2);
+
+
+let sumas,sumas2;
+sumas=24;
+sumas2=12;
+console.log(sumas+sumas2);
+
+let i=0;
+
+for(let i=0;i<=5;i++)
+    { 
+        if(parseInt(i)===2)
+            {alert("numero par")}
+        else if(parseInt(i)===3)
+        {alert("numero impar")}
+
+    console.log(i);
+}
+
+
+let nombre= prompt("decime tu nombre");
+{
+    alert("bienvenido "+nombre);
+
+}
