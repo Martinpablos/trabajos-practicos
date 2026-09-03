f6960ecceb84017f5345229fda30821321cb155d
Martin <tincho.pbs43@gmail.com>
2026-09-03 17:46:19 -0300

    actualizacion

--- javascript-modulo2/poo/objetos.js
+++ javascript-modulo2/poo/objetos.js
@@ -29,25 +29,48 @@
 
 // auto5.exibirInfo();
 
+//
+//
+//class receta {
+//    constructor (nombre, ingredientes, porciones ){
+//
+//        this.nombre=nombre;
+//        this.ingredientes=ingredientes;
+//        this.porciones=porciones;
+//        this.Info= `El plato es ${this.nombre} hecho con ${this.ingredientes} y se sirven ${this.porciones} porciones.`;
+//    }
+//    exibirInfo() {
+//        console.log(this.Info);
+//    }
+//}
+//
+//let receta1 =new receta ("pollo con salsa blanca y champignones", "pollo, crema,caldo,cebolla de verdeo, champignones,etc", 4);
+//let receta2 =new receta ("lomo al malbec", "lomo, malbec, sal, pimienta, aceite de oliva, ajo", 4);
+//let receta3 =new receta ("empanadas de carne cortadas a cuchillo", "bola de lomo, papas,cebolla,pimiento,aceitunas",12);
+//
+//receta1.exibirInfo();
+//receta2.exibirInfo();
+//receta3.exibirInfo();
+//
+//
+//
 
 
-class receta {
-    constructor (nombre, ingredientes, porciones ){
 
-        this.nombre=nombre;
-        this.ingredientes=ingredientes;
-        this.porciones=porciones;
-        this.Info= `El plato es ${this.nombre} hecho con ${this.ingredientes} y se sirven ${this.porciones} porciones.`;
-    }
-    exibirInfo() {
-        console.log(this.Info);
-    }
+
+let recetas = ('ingresa el numero de la receta que deseas ver: receta 1, receta 2, receta 3');
+const menu = prompt(recetas); 
+
+if (menu == '1') {
+    console.log("pollo con salsa blanca y champignones", "pollo, crema,caldo,cebolla de verdeo, champignones,etc");
+}
+else if (menu == '2'){
+    console.log("empanadas de carne cortadas a cuchillo", "bola de lomo, papas,cebolla,pimiento,aceitunas");
+}
+else if (menu == '3'){
+    console.log("lomo al malbec", "lomo, malbec, sal, pimienta, aceite de oliva, ajo");
+}
+else {
+    console.log('no existe la receta');
 }
 
-let receta1 =new receta ("pollo con salsa blanca y champignones", "pollo, crema,caldo,cebolla de verdeo, champignones,etc", 4);
-let receta2 =new receta ("lomo al malbec", "lomo, malbec, sal, pimienta, aceite de oliva, ajo", 4);
-let receta3 =new receta ("empanadas de carne cortadas a cuchillo", "bola de lomo, papas,cebolla,pimiento,aceitunas",12);
-
-receta1.exibirInfo();
-receta2.exibirInfo();
-receta3.exibirInfo();
